1abc66b534d82c7936c628b6d9a6b62162e6ba71
brainy-language-7409 <castlesofcode@gmail.com>
2019-02-06 21:09:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,14 +8,10 @@ function happyHolidaysTo (name) {
   return (`Happy holidays, ${name}!`)
 }
 
-function happyHolidayto (name, holiday) {
+function happyHolidayTo (name, holiday) {
   return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown (holiday, name) {
   return `It's ${days} days until ${holiday}!`
-}
-
-function happyHoliday(greeting, firstName) {
-  return `${greeting}, ${firstName}!`
 }
